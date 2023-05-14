e68170789d9e2a415e9293fbb8dd209ae5ca1994
minhaz000 <mrmminhaz@gmail.com>
2023-05-14 23:34:56 +0600

    hello

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React, { useEffect } from 'react';
+import axios from '../../hooks/axios.hook';
+// import axios from 'axios';
+function page(props:number) {
+
+  useEffect(()=>{
+    axios.get('/view').then(res=>{
+      console.log(res)
+    })
+  } ,[])
+  return (
+    <div>
+      hello this is chat page with
+    </div>
+  );
+}
+
+export default page;
